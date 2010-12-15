c975abe062f7e6400918d01233fb333cac83691d
root <root@debian-ruqqq.localdomain>
2010-12-15 12:29:58 +0800

    Initial Disk Writing implementation

--- bitflurry.h
+++ bitflurry.h
@@ -5,6 +5,10 @@
 #include "database.h"
 
 #define DISK_TOTAL 3
+#define DISK_PATH "disks/"
+// our disks
+//	*IMPORTANT* make sure the number of disk below equates to the DISK_TOTAL!!
+char *dirs[] = {"chocolate", "strawberry", "vanilla"};
 
 // initialization
 // 	needs to be run before using any other functions

8c3da1255d1ebdf33cfd5ed2ccf20bf3dfa20b40
Andrew <andrew@ubuntu.(none)>
2010-12-17 04:45:10 +0800

    Added missing raid0 code - syntax error on the struct

---
+++ raid0.h
@@ -0,0 +1,17 @@
+		
+#define BUFFER_SIZE 1048576 	 	// 1Mb 1048576
+#define CHUNK_SIZE 4*BUFFER_SIZE 	// We use 4MB as our chunk size
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <math.h>
+ expected ‘)’ before ‘*’ token
+int DISK_TOTAL;
+char DISK_PATH[50];
+int DISK_RAID;
+char **DISK_ARRAY;
+hhh	asdasd
+void raid0_putFileasdasd
+void raid0_getFile(struct bf_file *file, char *outfile);
